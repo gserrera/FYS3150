9b04cb4f375f27d017805f93a155d9d8de7565c5
gserrera <42637421+gserrera@users.noreply.github.com>
2018-12-06 21:46:49 +0100

    Update and rename PDE2.cpp to 2dimensional.cpp

--- Projects/Project_5/Programs/2dimensional.cpp
+++ Projects/Project_5/Programs/2dimensional.cpp
@@ -1,5 +1,3 @@
-// PDE1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 #include <cmath>
 #include <iostream>
 #include <fstream>
@@ -14,20 +12,15 @@ ofstream ofile;
 
 int main()
 {
-	//  First we set initialise the new and old vectors
-  //  Here we have chosen the boundary conditions to be zero.
-  //  n+1 is the number of mesh points in x
-  //  Armadillo notation for vectors
 	string filename;
 	cout << "Introduce the name of the output file: ";
 	cin >> filename;
-	int n = 50;
-	int tsteps = 10000;
-	double alpha = ((double)2500) / ((double)10000);
-
-	double step = 1 / n;
+	int n = 99;
+	int tsteps = 40000;
+	double alpha = (n+1)*(n+1) /((double)tsteps*tsteps);
+	//Creates matrices for allocating the results (in 2D) in a given moment and the next one
 	mat u(n + 1, n + 1); mat unew(n + 1, n + 1);
-	for (int i = 0; i <= n; i++) {
+	for (int i = 0; i <= n; i++) { //Boundary conditions
 		u(i, 0) = unew(i, 0) = u(i, n) = unew(i, n) = u(n, i) = unew(n, i) = 0.0;
 		u(0, i) = unew(0, i) = 1.0;
 	}
@@ -35,7 +28,7 @@ int main()
 		for (int j = 1; j < n; j++) {
 			//  initial condition
 			u(i,j) = 0.0;
-			//  intitialise the new vector 
+			//  intitialise the new matrix 
 			unew(i,j) = 0.0;
 		}
 	}
@@ -48,7 +41,7 @@ int main()
 	for (int t = 1; t <= tsteps; t++) {
 		for (int i = 2; i < n; i++) {
 			for (int j = 2; j < n; j++) {
-				// Discretized diff eq
+				// Discretized diff equation and prints results
 				unew(i, j) = (1 - 4 * alpha) * u(i, j) + alpha * (u(i + 1, j) + u(i - 1, j) + u(i, j + 1) + u(i, j - 1));
 
 				ofile << setw(15) << setprecision(8) << t;
@@ -59,10 +52,10 @@ int main()
 		}
 		for (int i2 = 1; i2 < n; i2++) {
 			for (int j2 = 1; j2 < n; j2++) {
-				u(i2, j2) = unew(i2, j2);
+				u(i2, j2) = unew(i2, j2); //Replaces old matrix with new for the next time step
 			}
 		}
-		//  note that the boundaries are not changed.
+		
 	}
 	ofile.close();  // close output file
 	return 0;
